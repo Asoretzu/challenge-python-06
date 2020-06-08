670a28219f3dd7e60e29ef38e0fdaf1dfe887ee3
Asoretzu <asoretzu@gmail.com>
2020-06-08 18:27:36 -0500

    Challenge completed.

--- challenge.py
+++ challenge.py
@@ -2,8 +2,11 @@ def make_division_by(n):
     """This closure returns a function that returns the division
        of an x number by n 
     """
-    # You have to code here!
-    pass
+    def to_divide(x):
+        assert type(x) == int  or type(x) == float, "You can only use numbers."
+        return x / n
+
+    return to_divide
 
 
 def run():
@@ -21,8 +24,22 @@ if __name__ == '__main__':
     import unittest
 
     class ClosureSuite(unittest.TestCase):
-        def test_closure_make_division_by(self):
-            # Make the closure test here
-            pass
+        def setUp(self):
+            self.divisions = {
+                6: [3, 18],
+                20: [5, 100],
+                3: [18, 54],
+                5: [3.0, 15.0],
+                20.22: [5, 101.1],
+            }
 
-    run()
+        def test_closure_make_division_by(self):
+            for key, values in self.divisions.items():
+                self.assertEqual(key, make_division_by(values[0])(values[1]))
+        
+        def tearDown(self):
+            del(self.divisions)
+
+
+    unittest.main()
+    # run()
